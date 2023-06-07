32e0284d37cda761d46fc97fee0719871bef7898
Manish Baral <mail.aarman@gmail.com>
2023-06-07 14:30:52 +0545

    initial setup

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  singleQuote: false,
+  printWidth: 120,
+  semi: true,
+  endOfLine: "auto",
+  tabWidth: 2,
+  arrowParens: "avoid",
+};
